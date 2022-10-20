d7d5574af8b95a35b3eadbb1d780e4afa1de1dad
igor.guariroba <igorguariroba@movida.com.br>
2022-10-20 16:44:48 -0300

    Modal para inserir os preços

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
-import { HeaderContainer, HeaderContent, LogoImage,LogoContainer, NewTransactionsButton } from "./styles";
+import { HeaderContainer, HeaderContent, LogoContainer, NewTransactionsButton } from "./styles";
 import { Wallet } from "phosphor-react";
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <LogoContainer>
-        <Wallet size={40} color='#96c95c' />
-        <span>Money</span>
+          <Wallet size={40} color='#96c95c' />
+          <span>Money</span>
         </LogoContainer>
-        <NewTransactionsButton>Nova transação</NewTransactionsButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionsButton>Nova transação</NewTransactionsButton>
+          </Dialog.Trigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
