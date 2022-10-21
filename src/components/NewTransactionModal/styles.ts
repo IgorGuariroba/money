aa19c6482ba5371f0098efe679416367634dc749
igor.guariroba <igorguariroba@movida.com.br>
2022-10-20 21:17:54 -0300

    Trasações

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -98,12 +98,13 @@ export const TransactionTypeButton = styled(RadioGroup.Item)<TransactionTypeButt
     }
 
     &[data-state='unchecked']:hover{
+      transition: background-color 0.2s;
       background: ${props => props.theme['gray-600']};
     }
 
     &[data-state='checked']{
       color: ${props => props.theme.white};
-      background: ${props => props.variant === 'income' ? props.theme['green-300'] : props.theme['red-500']};
+      background: ${props => props.variant === 'income' ? props.theme['green-500'] : props.theme['red-500']};
 
       svg {
         color: ${props => props.theme['white']};
