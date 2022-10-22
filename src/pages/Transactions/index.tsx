0d37033f0c4352414111c93ccdf789c4a8e7b324
igor.guariroba <igorguariroba@movida.com.br>
2022-10-22 15:51:10 -0300

    Conexão com a api

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,42 +1,60 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { DescriptionComponent, PriceHighlight, TransactionContainer, TransactionTable } from "./styles";
 
+interface Transaction {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  created_at: string;
+}
+
 export function Transactions() {
+
+  const [transactions, setTransactions] = useState<Transaction[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3000/transactions')
+    const data = await response.json()
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+
   return (
     <div>
       <Header />
       <Summary />
-
       <TransactionContainer>
-        <SearchForm/>
+        <SearchForm />
+
         <TransactionTable>
           <tbody>
-            <tr>
-              <DescriptionComponent>Desenvovimento de Site</DescriptionComponent>
-              <td>
-                <PriceHighlight variant="income">
-                  R$ 12.000.00
-                </PriceHighlight>
-              </td>
-              <td>Venda</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <DescriptionComponent>Hamburger</DescriptionComponent>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 12.000.00
-                </PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>13/04/2022</td>
-            </tr>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <DescriptionComponent>{transaction.description}</DescriptionComponent>
+                  <td>
+                    <PriceHighlight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighlight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>{transaction.created_at}</td>
+                </tr>
+              )
+            })}
           </tbody>
         </TransactionTable>
-      </TransactionContainer>
 
+      </TransactionContainer>
     </div>
   )
 }
