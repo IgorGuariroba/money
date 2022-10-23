33652793a83b8c02898ba08d892d4ba66a407239
igor.guariroba <igorguariroba@movida.com.br>
2022-10-22 21:16:16 -0300

    Transações estão sendo filtradas buscando no servidor

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -12,6 +12,7 @@ interface Transaction {
 
 interface TransactionContextType {
   transactions: Transaction[];
+  fetchTransactions: (query: string) => Promise<void>;
 }
 
 interface TransactionProviderProps {
@@ -24,18 +25,25 @@ export function TransactionProvider({ children }: TransactionProviderProps) {
 
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3000/transactions')
+  async function fetchTransactions( query?: string) {
+    const url = new URL('http://localhost:3000/transactions')
+    if(query){
+      url.searchParams.append('q', query)
+    }
+    const response = await fetch(url)
     const data = await response.json()
     setTransactions(data)
   }
 
   useEffect(() => {
-    loadTransactions()
+    fetchTransactions()
   }, [])
 
   return (
-    <TransactionsContext.Provider value={{ transactions }}>
+    <TransactionsContext.Provider value={{ 
+      transactions,
+      fetchTransactions,
+      }}>
       {children}
     </TransactionsContext.Provider>
   )
